3000d4d7f71036e5f9ace84d5b2bedf00a2235a0
LeafHacker <leafhacker@yahoo.com>
2019-12-23 05:18:04 +0000

    Theme colours

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,14 +1,14 @@
-import { createMuiTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
+import { createMuiTheme } from '@material-ui/core/styles';
 
-// Create a theme instance.
-const theme = createMuiTheme({
+// Export a theme instance.
+export default createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#2196F3',
     },
     secondary: {
-      main: '#19857b',
+      main: '#039be5',
     },
     error: {
       main: red.A400,
@@ -17,6 +17,4 @@ const theme = createMuiTheme({
       default: '#fff',
     },
   },
-});
-
-export default theme;
+});;
