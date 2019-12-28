a6074addbf0e6d8d3405210c22afe8cc8e20cad3
EvilSourcerer <evilsourcerer@gmail.com>
2019-12-27 20:13:57 -0500

    Remove ProTip

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,6 @@ import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
 import MuiLink from '@material-ui/core/Link'
 
-import ProTip from '../components/ProTip'
 import Staff from '../components/Staff'
 import Layout from '../components/Layout'
 
@@ -14,7 +13,6 @@ export default function About(): JSX.Element {
 				<Box my={4}>
 					<MuiLink href="/">Go to the main page</MuiLink>
 					<Staff />
-					<ProTip />
 				</Box>
 			</Container>
 		</Layout>
