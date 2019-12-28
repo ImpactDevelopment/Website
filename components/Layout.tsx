d5592c29fa11aa1f0f2a3ac72ac85bf5de8a26fa
EvilSourcerer <evilsourcerer@gmail.com>
2019-12-27 21:12:54 -0500

    Add dark mode theme with toggle

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,38 @@
-import { AppBar, Fab, Toolbar, Typography, ButtonBase } from '@material-ui/core'
+import { AppBar, ButtonBase, Fab, IconButton, makeStyles, Toolbar, Typography } from '@material-ui/core'
 import Link from 'next/link'
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
 import Head from 'next/head'
-import React from 'react'
+import React, { useContext } from 'react'
+import { InvertColors } from '@material-ui/icons'
 
 import ElevationScroll from './ElevationScroll'
 import ScrollTop from './ScrollTop'
 import Copyright from './Copyright'
+import { AppContext } from './AppContext'
 
 interface Props {
 	children: React.ReactElement
 	title?: string
 }
 
+const useStyles = makeStyles(theme => ({
+	grow: {
+		flexGrow: 1,
+	},
+	navLink: {
+		marginLeft: theme.spacing(2),
+	},
+}))
+
 export default function Layout({ children, title = 'Impact' }: Props): JSX.Element {
+	const { themeType, setThemeType } = useContext(AppContext)
+	const classes = useStyles()
+
+	const toggleTheme = (): void => {
+		if (setThemeType) {
+			setThemeType(themeType === 'dark' ? 'light' : 'dark')
+		}
+	}
 	return (
 		<>
 			<Head>
@@ -27,6 +46,10 @@ export default function Layout({ children, title = 'Impact' }: Props): JSX.Eleme
 								<Typography variant="h5">{title}</Typography>
 							</Link>
 						</ButtonBase>
+						<div className={classes.grow} />
+						<IconButton color="inherit" onClick={toggleTheme}>
+							<InvertColors />
+						</IconButton>
 					</Toolbar>
 				</AppBar>
 			</ElevationScroll>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,30 @@ import { ThemeProvider } from '@material-ui/core/styles'
 import App from 'next/app'
 import Head from 'next/head'
 import React from 'react'
+import { PaletteType } from '@material-ui/core'
 
-import theme from '../components/theme'
+import makeTheme from '../components/theme'
+import { AppContext } from '../components/AppContext'
+
+interface State {
+	themeType: PaletteType
+}
 
 // `App` component is used for initialize of pages. It allows for overwriting and full control of the `page` initialization.
 // This allows for keeping state between navigation, custom error handling, injecting additional data.
 export default class MyApp extends App {
+	state: State = {
+		themeType: 'dark',
+	}
+
+	// this is an arrow function because `this` is overwritten by the class.
+	setThemeType = (type: PaletteType): void => {
+		this.setState({
+			...this.state,
+			themeType: type,
+		})
+	}
+
 	componentDidMount(): void {
 		// Remove the server-side injected CSS.
 		const jssStyles = document.querySelector('#jss-server-side')
@@ -19,17 +37,26 @@ export default class MyApp extends App {
 
 	render(): JSX.Element {
 		const { Component, pageProps } = this.props
+		const theme = makeTheme(this.state.themeType)
 
 		return (
 			<>
 				<Head>
 					{/* https://err.sh/next.js/no-document-title */}
-					<title>My new cool app</title>
+					<title>Impact</title>
+					<meta name="theme-color" content={theme.palette.primary.main} />
 				</Head>
 				<ThemeProvider theme={theme}>
 					{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
 					<CssBaseline />
-					<Component {...pageProps} />
+					<AppContext.Provider
+						value={{
+							themeType: this.state.themeType,
+							setThemeType: this.setThemeType,
+						}}
+					>
+						<Component {...pageProps} />
+					</AppContext.Provider>
 				</ThemeProvider>
 			</>
 		)
